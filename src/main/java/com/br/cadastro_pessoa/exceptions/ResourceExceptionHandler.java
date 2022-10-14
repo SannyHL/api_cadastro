e70f174c741fcb39a5066bc16a8bc12bef35f18f
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 19:44:05 -0300

    Adição de configuração da exceção.

---
+++ src/main/java/com/br/cadastro_pessoa/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.br.cadastro_pessoa.exceptions;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// Classe para personalização da mensagem da exceção de objeto não encontrado
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request){
+        var error = new StandardError(LocalDateTime.now(ZoneId.of("UTC-3")), HttpStatus.NOT_FOUND.value(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
