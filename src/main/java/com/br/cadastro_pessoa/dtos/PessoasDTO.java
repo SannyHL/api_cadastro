84af9df3114b81fa67c27bc9330266661a8a2c8b
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 19:12:54 -0300

    Criação de classe DTO e inserção de métodos.

---
+++ src/main/java/com/br/cadastro_pessoa/dtos/PessoasDTO.java
@@ -0,0 +1,27 @@
+package com.br.cadastro_pessoa.dtos;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PessoasDTO {
+
+    private Long id;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDeNascimento;
+    @NotBlank
+    private String email;
+    
+}
