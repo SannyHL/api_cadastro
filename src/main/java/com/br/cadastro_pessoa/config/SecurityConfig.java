3208173b021a034c2705f804166bd0d2faeaf950
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 20:11:11 -0300

    Configuração do security.

---
+++ src/main/java/com/br/cadastro_pessoa/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.br.cadastro_pessoa.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .cors()
+        .and()
+        .httpBasic()
+        .and()
+        .authorizeRequests().antMatchers("/pessoa**").permitAll().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("api")
+        .password("{noop}123")
+        .roles("roles");
+}
+    
+}
