1d596faf3b362845485b769d2c6df26b477d1cf9
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 19:22:16 -0300

    Criação de Interface para PessoaService.

---
+++ src/main/java/com/br/cadastro_pessoa/interfaces/PessoaServiceInterface.java
@@ -0,0 +1,14 @@
+package com.br.cadastro_pessoa.interfaces;
+
+import java.util.List;
+
+import com.br.cadastro_pessoa.dtos.PessoaDTO;
+import com.br.cadastro_pessoa.models.PessoaModel;
+
+public interface PessoaServiceInterface {
+    
+    List<PessoaModel> findAll();
+    PessoaModel findById(Long id);
+    PessoaModel create(PessoaDTO pessoaDTO);
+    
+}
