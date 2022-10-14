d192592ac986742ce32d4199bdffb6032b21f34f
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 19:10:03 -0300

    Criação de classe e adição de atributos.

---
+++ src/main/java/com/br/cadastro_pessoa/models/PessoasModel.java
@@ -0,0 +1,30 @@
+package com.br.cadastro_pessoa.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="pessoa")
+public class PessoasModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column
+    private Long id;
+
+    private String nome;
+
+    private LocalDate dataDeNascimento;
+
+    @Email
+    private String email;
+}
