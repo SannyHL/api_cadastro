f654710c461e2a35757096a851f290282effbe75
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 21:41:59 -0300

    Criação de método Get(findAll)

--- src/main/java/com/br/cadastro_pessoa/controllers/PessoaController.java
+++ src/main/java/com/br/cadastro_pessoa/controllers/PessoaController.java
@@ -2,10 +2,13 @@ package com.br.cadastro_pessoa.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +36,7 @@ public class PessoaController {
     private ModelMapper mapper;
     
     @PostMapping
-    @ApiOperation("Criar dados")
+    @ApiOperation("Criar cadastro")
     public ResponseEntity<PessoaDTO> create(@RequestBody PessoaDTO pessoaDTO){
         PessoaModel pessoaModel = service.create(pessoaDTO);
         URI uri = ServletUriComponentsBuilder
@@ -43,6 +46,16 @@ public class PessoaController {
         .toUri();
         return ResponseEntity.created(uri).build();
     } 
+
+    @GetMapping
+    @ApiOperation("Listar todas as pessoas")
+    public ResponseEntity<List<PessoaDTO>> findAll(){
+        return ResponseEntity.ok()
+        .body(service.findAll()
+        .stream().map(objetos -> mapper.map(objetos, PessoaDTO.class))
+        .collect(Collectors.toList()));
+    }
+
     
 
     
