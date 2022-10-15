991f7def60e9369b29ba4b10b2461edbf0f7a0c1
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-14 22:06:02 -0300

    Inserção update.

--- src/main/java/com/br/cadastro_pessoa/controllers/PessoaController.java
+++ src/main/java/com/br/cadastro_pessoa/controllers/PessoaController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,7 +66,14 @@ public class PessoaController {
     @ApiOperation("Busca por Id")
     public ResponseEntity<PessoaDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok().body(mapper.map(service.findById(id), PessoaDTO.class));
-        
+    }
+
+    //Método para atualizar cadastro
+    @PutMapping(ID)
+    @ApiOperation("Atualizar")
+    public ResponseEntity<PessoaDTO> update(@PathVariable Long id, @RequestBody PessoaDTO pessoaDTO){
+        pessoaDTO.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(pessoaDTO), PessoaDTO.class));
     }
 
     
